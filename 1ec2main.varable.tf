6df294cda589d8c25771d552162feae2f08cb321
DevOps-Factory <factorydevops@gmail.com>
2025-09-04 14:27:39 +0530

    Update 1ec2main.varable.tf

--- 1ec2main.varable.tf
+++ 1ec2main.varable.tf
@@ -32,3 +32,17 @@ resource "aws_instance" "my_ec2" {
     Name = "rakeshperala"   
   }
 }
+#-------------------------
+# output
+#------------------------
+output "ec2_public_ip" {
+  value = aws_instance.my_ec2.public_ip
+}
+
+output "ec2_public_dns" {
+  value = aws_instance.my_ec2.public_dns
+}
+
+output "ec2_private_ip" {
+  value = aws_instance.my_ec2.private_ip
+}
