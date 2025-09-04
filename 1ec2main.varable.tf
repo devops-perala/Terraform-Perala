8b61f7519ee83dc09d709eb1ce8527b90616791f
DevOps-Factory <factorydevops@gmail.com>
2025-09-04 13:52:13 +0530

    Create 1ec2main.varable.tf

---
+++ 1ec2main.varable.tf
@@ -0,0 +1,34 @@
+# --------------------
+variable "aws_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "aws_root_storage_size" {
+  type    = number
+  default = 15
+}
+
+variable "ec2_ami_id" {
+  type    = string
+  default = "ami-0360c520857e3138f"
+}
+
+# --------------------
+# EC2 Instance
+# --------------------
+resource "aws_instance" "my_ec2" {
+  ami                    = var.ec2_ami_id
+  instance_type          = var.aws_instance_type
+  key_name               = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.my_sg_group.id]   
+
+  root_block_device {
+    volume_size = var.aws_root_storage_size
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "rakeshperala"   
+  }
+}
